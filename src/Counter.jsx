60ea0d59cb608d1f8e79f9bc7958e30ec0d85333
Aleksandr Savchenko <s1a9v8a5@ukr.net>
2021-02-23 17:32:53 +0200

    commit

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 
-const Counter = () => {
+const Counter = ({textInputOne, textInputTwo, count, counterPlayerOne}) => {
 
     let a = "Player 1 :";
     let b = "Player 2 :";
     return (
         <div className="counter">
-            {/*<span className="score">Score</span>*/}
-            <span className="firstName">{a} {printNameOne}</span>
-            <span className="secondName">{b} {printNameTwo}</span>
+            <span className="score">Score</span>
+            <span className="firstName">  {textInputOne} : {count} </span>
+            <span className="secondName">  {textInputTwo} : {count} </span>
 
         </div>
-)
+    )
 };
 
 
--- src/Game.jsx
+++ src/Game.jsx
@@ -1,12 +1,39 @@
 import React, {useState} from 'react';
 import Board from "./Board";
 import {calculateWinner} from "./helper";
+import Popup from "./Popup";
+import Counter from "./Counter";
 
 
 const Game = () => {
-    const [board, setBoard] = useState(Array(9).fill(null))
-    const [xIsNext, setXIsNext] = useState(true)
-    const winner = calculateWinner(board)
+    const [board, setBoard] = useState(Array(9).fill(null));
+    const [xIsNext, setXIsNext] = useState(true);
+    const [isOpen, setIsOpen] = useState(true);
+    const [textInputOne, setTextInputOne] = useState("");
+    const [textInputTwo, setTextInputTwo] = useState("");
+    const [count, setCount] = useState(0)
+    const winner = calculateWinner(board);
+
+    const counterPlayerOne = () => {
+        return setCount(count + 1)
+    }
+    const hidePop = () => {
+        setIsOpen(false)
+    };
+    const printNameOne = () => {
+        let nameOne = document.querySelector(".player1").value;
+        setTextInputOne(nameOne)
+    };
+    const printNameTwo = () => {
+        let nameTwo = document.querySelector(".player2").value;
+        setTextInputTwo(nameTwo)
+    }
+    let print = () => {
+        printNameOne()
+        printNameTwo()
+        hidePop()
+    }
+
 
     const handleClick = (index) => {
         const boardCopy = [...board]
@@ -21,17 +48,35 @@ const Game = () => {
 
     const startNewGame = () => {
         return (
-            <button className="start__btn" onClick={() => setBoard(Array(9).fill(null))}> Очистить поле </button>
+            <button className="start__btn" onClick={() => setBoard(Array(9).fill(null))}> Clear board </button>
+        )
+    }
+    const newGame = () => {
+        return (<button className="start__btn" onClick={() => location.reload()}>New game</button>
+
         )
     }
 
 
     return (
         <div className="wrapper">
-            { startNewGame() }
-            <Board squares={board} click={handleClick} />
+            <Popup
+                print={print}
+                isOpen={isOpen}
+
+            />
+            <Counter
+                textInputOne={textInputOne}
+                textInputTwo={textInputTwo}
+                count={count}
+                counterPlayerOne={counterPlayerOne}
+
+            />
+            {startNewGame()}
+            {newGame()}
+            <Board squares={board} click={handleClick}/>
             <p className="game__info">
-                { winner ? 'Победитель ' + winner : 'Сейчас ходит ' + ( xIsNext ? 'X' : 'O') }
+                {winner ? 'Победитель ' + (winner === "X" ? textInputOne : textInputTwo) : 'Сейчас ходит ' + (xIsNext ? textInputOne : textInputTwo)}
             </p>
         </div>
     )
